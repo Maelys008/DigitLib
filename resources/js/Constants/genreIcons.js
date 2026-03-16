618845bb01c250fa3b665ce7bac1a6f67a7eae62
OSSENI Rosmiyath <rosmiros52@gmail.com>
2026-03-16 17:43:57 +0100

    update: simulation des donéeset utilisation des icone de lucide-react

---
+++ resources/js/Constants/genreIcons.js
@@ -0,0 +1,43 @@
+// resources/js/constants/genreIcons.js
+
+import {
+  BookOpen,      // Fiction
+  FlaskConical,  // Science
+  ScrollText,    // Histoire
+  Library,       // Classique
+  Skull,         // Mystère
+  Sparkles,      // Fantasy
+  Heart,         // Romance
+  Ghost,         // Horreur
+  UserRound,     // Biographie
+  Brain,         // Philosophie
+  Feather,       // Poésie
+  Compass,       // Voyage
+  UtensilsCrossed, // Cuisine
+  Code,          // Programmation
+  Palette,       // Art
+  Baby,          // Jeunesse
+  Swords,        // Thriller
+  Sparkle        // Développement personnel
+} from "lucide-react";
+
+export const genreIcons = {
+  'Fiction': BookOpen,
+  'Science': FlaskConical,
+  'Histoire': ScrollText,
+  'Classique': Library,
+  'Mystère': Skull,
+  'Fantasy': Sparkles,
+  'Romance': Heart,
+  'Horreur': Ghost,
+  'Biographie': UserRound,
+  'Philosophie': Brain,
+  'Poésie': Feather,
+  'Voyage': Compass,
+  'Cuisine': UtensilsCrossed,
+  'Programmation': Code,
+  'Art': Palette,
+  'Jeunesse': Baby,
+  'Thriller': Swords,
+  'Développement personnel': Sparkle
+};
