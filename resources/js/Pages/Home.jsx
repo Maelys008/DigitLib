764935e8096ffd100e3c9bc46c092e6d11cf79c5
OSSENI Rosmiyath <rosmiros52@gmail.com>
2026-03-23 09:51:48 +0100

    Amélioration de la page d'acuil principale

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -4,36 +4,17 @@ import TopBar from '@/Components/TopBar';
 import SectionHeader from '@/Components/SectionHeader';
 import BookCard from '@/Components/BookCard';
 import HorizontalScroll from '@/Components/HorizontalScroll';
-import { getPastelColor } from '@/Constants/colors';
 import { genreIcons } from '@/Constants/genreIcons';
 import GenreCard from '@/Components/GenreCard';
 import NewBookCard from '@/Components/NewBookCard';
-import EditeurChip from '@/Components/EditeurChip';
-import VerticalScroll from '@/Components/VerticalScroll';
 import { getBookCountByGenre } from '../utils/getBookCountByGenre';
+
 export default function Home() {
   // Données pour les différentes sections
-	const livresMemmbres = livres.filter(l => l.nb_disponibles > 0).slice(2, 17);
+	const livresMembres = livres.filter(l => l.nb_disponibles > 0).slice(2, 17);
   const livresPopulaires = livres.filter(l => l.nb_disponibles > 0).slice(5, 17);
   const livresNouveaux = livres.slice(-6);
   const livresRomans = livres.filter(l => l.genre === 'Roman').slice(0, 17);
-	
-  
-  // Maisons d'édition 
-const maisonsEdition = [
-  { id: 1, nom: 'BOMBORA' },
-  { id: 2, nom: 'ROSEM' }, 
-  { id: 3, nom: 'GALLIMARD' },
-  { id: 4, nom: 'ACTES SUD' },
-  { id: 5, nom: 'SEUIL' },
-  { id: 6, nom: 'FLAMMARION' },
-  { id: 7, nom: 'ALBIN MICHEL' },
-  { id: 8, nom: 'STOCK' },
-  { id: 9, nom: 'GRASSET' },
-  { id: 10, nom: 'MINUIT' },
-  { id: 11, nom: 'POL' },
-  { id: 12, nom: 'DENOËL' }
-];
   const genresAccueil = genres.slice(0, 6);
   const notificationsNonLues = notifications.filter(n => n.statut === 'non_lu');
   return (
@@ -47,12 +28,11 @@ const maisonsEdition = [
       <div className="px-6 mt-2">
         <SectionHeader titre="Meilleurs du mois" voirToutLien="best-of-month" />
         <HorizontalScroll>
-          {livresMemmbres.slice(0, 15).map((livre, index) => (
+          {livresMembres.slice(0, 15).map((livre, index) => (
             <BookCard
               key={livre.id}
               livre={livre}
               variant="horizontal"
-              backgroundColor={getPastelColor(index)}
             />
           ))}
         </HorizontalScroll>
@@ -91,17 +71,7 @@ const maisonsEdition = [
 				</div>
 			</div>
 
-      {/* SECTION 4: Maisons d'édition */}
-				<div className="px-6 mt-6">
-					<SectionHeader titre="Maisons d'édition" voirToutLien="/editeurs" />
-					<HorizontalScroll>
-						{maisonsEdition.map((editeur, index) => (
-							<EditeurChip key={editeur.id} isActive={index === 0}>
-								{editeur.nom}
-							</EditeurChip>
-						))}
-					</HorizontalScroll>
-				</div>
+   
 
       {/* SECTION 5: Populaires */}
       <div className="px-6 mt-6">
@@ -112,7 +82,6 @@ const maisonsEdition = [
               key={livre.id}
               livre={livre}
               variant="horizontal"
-              backgroundColor={getPastelColor(index + 12)}
             />
           ))}
         </HorizontalScroll>
@@ -128,7 +97,6 @@ const maisonsEdition = [
                 key={livre.id}
                 livre={livre}
                 variant="horizontal"
-                backgroundColor={getPastelColor(index + 9)}
               />
             ))
           ) : (
@@ -137,7 +105,6 @@ const maisonsEdition = [
                 key={livre.id}
                 livre={livre}
                 variant="horizontal"
-                backgroundColor={getPastelColor(index + 9)}
               />
             ))
           )}
